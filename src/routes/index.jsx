771447c486c75df4eb77cac34bd7d3d5ed5634d9
mdabarik <mdabarik19@gmail.com>
2023-11-01 21:54:56 +0600

    about contact route added

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,22 @@
 import { createBrowserRouter  } from "react-router-dom";
 import App from "../App";
+import Contact from "../page/Contact";
+import About from "../page/About";
 
 const routes = createBrowserRouter([
     {
         path: '/',
-        element: <App></App>
+        element: <App></App>,
+        children: [
+            {
+                path: '/about',
+                element: <About></About>
+            },
+            {
+                path: '/contact',
+                element: <Contact></Contact>
+            }
+        ]
     }
 ])
 
